bff25e017609b26ec4c909107d6e93ac948e4f7d
Lizzie <leejihye7117@gmail.com>
2023-04-27 13:15:45 +0900

    header 반응형 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,32 +7,63 @@ export default function Header() {
   return (
     <header>
       <HeaderWrapper>
-        <div className="text-box" style={{ textAlign: "left" }}>
+        <div
+          style={{
+            textAlign: "left",
+            display: "flex",
+            justifyContent: "flex-end",
+          }}
+        >
           <Image
-            src="/quotation_marks.svg"
-            alt="quotation_marks"
-            width={50}
-            height={50}
+            className="background-image"
+            src="/title_background_top.png"
+            alt="title_background_top"
+            width={798}
+            height={386}
           />
+        </div>
+        <Image
+          className="background-image"
+          src="/title_background_bottom.png"
+          alt="title_background_top"
+          width={840}
+          height={662}
+          style={{
+            bottom: "0",
+            left: "0",
+          }}
+        />
+        <div className="text-container">
+          <div className="text-box" style={{ textAlign: "left" }}>
+            <Image
+              src="/quotation_marks.svg"
+              alt="quotation_marks"
+              width={50}
+              height={50}
+            />
+            <div className="bold-text-container">
+              <span className="bold-text">스티커를 활용한</span>
+              <span className="bold-text">칭찬 챌린지!</span>
+            </div>
+          </div>
+          <div className="text-box" style={{ textAlign: "center" }}>
+            <div className="text-line">
+              <span className="text">가족, 친구가 좋은 일을 할때마다</span>
+              <span className="text"> 스티커 하나씩!</span>
+            </div>
+            <div className="text-line">
+              <span className="text">목표를 설정하고 이루어질 때마다</span>
+              <span className="text"> 나에게 하나씩!</span>
+            </div>
+          </div>
+          <div className="text-box" style={{ textAlign: "right" }}>
+            <span className="bold-text">오늘도 당신을 응원해요!</span>
+          </div>
 
-          <p className="bold-text">
-            스티커를 활용한
-            <br />
-            칭찬 챌린지!
-          </p>
-        </div>
-        <div className="text-box" style={{ textAlign: "center" }}>
-          <p className="text">
-            가족, 친구가 좋은 일을 할때마다 스티커 하나씩!
-            <br />
-            목표를 설정하고 이루어질 때마다 나에게 하나씩!
-          </p>
-        </div>
-        <div className="text-box" style={{ textAlign: "right" }}>
-          <p className="bold-text">오늘도 당신을 응원해요!</p>
-          <div style={{ alignItems: "center" }}>
+          <div className="download-container">
             <Link href="https://play.google.com/store/apps/details?id=io.cocho.goodjob.goodjobapp&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1/?utm_source=googleplay&utm_medium=referral&utm_campaign=GoodJob">
               <Image
+                className="download-image"
                 alt="google_play_download"
                 src="/google_play_download.png"
                 width={215}
@@ -42,11 +73,11 @@ export default function Header() {
 
             <Link href="https://apps.apple.com/kr/app/goodjob-%EC%B0%B8%EC%9E%98%ED%96%88%EC%96%B4%EC%9A%94-%EC%8A%A4%ED%8B%B0%EC%BB%A4-%EC%B9%AD%EC%B0%AC-%EC%B1%8C%EB%A6%B0%EC%A7%80/id6446052088/?utm_source=ios&utm_medium=referral&utm_campaign=GoodJob">
               <Image
+                className="download-image"
                 alt="app_store_download"
                 src="/app_store_download.svg"
                 width={215}
                 height={83}
-                style={{ margin: "0 30px 0 20px" }}
               />
             </Link>
           </div>
--- src/styles/Wrapper.tsx
+++ src/styles/Wrapper.tsx
@@ -1,21 +1,132 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
-  background-image: url("/title_background.svg");
   height: 100vh;
   width: 100vw;
-  padding: 100px 120px 0 120px;
+  overflow: hidden;
+
+  .background-image {
+    position: absolute;
+    z-index: -1;
+  }
+
+  .text-container {
+    padding: 30px 10% 0 10%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+  }
+
+  .bold-text-container {
+    display: flex;
+    flex-direction: column;
+  }
 
   .bold-text {
     font-size: 50px;
     font-weight: bold;
+    text-shadow: -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff,
+      1px 1px 0 #fff;
   }
 
   .text {
     font-size: 40px;
+    text-shadow: -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff,
+      1px 1px 0 #fff;
   }
 
   .text-box {
     margin-bottom: 20px;
+  }
+
+  .download-container {
+    text-align: right;
+  }
+
+  .download-image {
+    margin-right: 27px;
+  }
+
+  @media screen and (max-width: 850px) {
+    .background-image {
+      width: 100%;
+      height: auto;
+    }
+  }
+
+  /* Tablets Size */
+  @media screen and (max-width: 768px) {
+    .text-container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      align-items: center;
+    }
+
+    .bold-text-container {
+      text-align: center;
+    }
+
+    .bold-text {
+      font-size: 43px;
+    }
+
+    .text-line {
+      display: flex;
+      flex-direction: column;
+    }
+
+    .text {
+      font-size: 33px;
+    }
+
+    .download-container {
+      text-align: center;
+    }
+
+    .download-image {
+      margin: 5px;
+    }
+  }
+
+  /* Phone Size */
+  @media screen and (max-width: 580px) {
+    .bold-text {
+      font-size: 38px;
+    }
+
+    .text {
+      font-size: 28px;
+    }
+
+    .download-container {
+      display: flex;
+      justify-content: center;
+      text-align: center;
+      align-items: center;
+    }
+
+    .download-image {
+      width: 180px;
+      height: auto;
+    }
+  }
+
+  /* Small Phone Size */
+  @media screen and (max-width: 480px) {
+    .bold-text {
+      font-size: 32px;
+    }
+
+    .text {
+      font-size: 22px;
+    }
+
+    .download-image {
+      width: 150px;
+      height: auto;
+    }
   }
 `;
