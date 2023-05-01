af3648c0f326146c39936385c78bc8a825b5b7da
Lizzie <leejihye7117@gmail.com>
2023-05-02 00:42:24 +0900

    title, meta description 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,17 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>굿잡</title>
+        <title>
+          굿잡 goodjob | 가족, 친구와 함께 즐기는 스티커를 활용한 칭찬 챌린지
+          애플리케이션
+        </title>
         <meta
           name="description"
-          content="굿잡 | 가족, 친구와 함께 즐기는 스티커를 활용한 칭찬 챌린지 애플리케이션"
+          content="💪 스티커를 활용한 칭찬 챌린지! 🙂 가족, 친구가 좋은 일을 할때마다 스티커 하나씩! 목표를 설정하고 이루어질 때마다 나에게 하나씩! 오늘도 당신을 응원해요! 👍, 칭찬 스티커, 참 잘했어요, 챌린지, 칭찬 어플리케이션"
+        />
+        <meta
+          name="google-site-verification"
+          content="dzK6vbDsyX99RnB3_bEyP5lsP1ZkaNTgfvnrjkPE9nA"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
